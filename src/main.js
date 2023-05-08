455c8221b3a10d1f0f988c54a3a3f4c85b34fa34
josiedpb14 <110139188+josiedpb14@users.noreply.github.com>
2023-05-08 05:04:20 +0000

    mimi

--- src/main.js
+++ src/main.js
@@ -1,28 +1,12 @@
-var button = document.getElementById("myButton");
-button.addEventListener("click",makeAnExcuse);
 function makeAnExcuse() {
   let num = Math.floor(Math.random() * 6) + 1;
   let excuse = "AAAA";
   document.getElementById("excuse").innerText = excuse;
 }
 
-let who = [
-  "My mom",
-  "Jeffry",
-  "My Dog",
-  "Mariano",
-  "A warewolf",
-  "My neighbor"
-];
+let who = ["My mom","Jeffry","My Dog","Mariano","A warewolf","My neighbor"];
 let action = ["stole", "scratched", "made", "drank", "lost", "broke"];
 let what = ["my phone", "your car", "a mess", "coke", "its prey", "the window"];
-let when = [
-  "while i was cooking",
-  "When you werent looking",
-  "during the evening",
-  "while we werent looking",
-  "when was asleep",
-  "when no one could see him"
-];
+let when = ["while i was cooking","When you werent looking","during the evening","while we werent looking","when was asleep","when no one could see him"];
 
 
