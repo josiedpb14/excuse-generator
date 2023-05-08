215ce6003e9820946df6d39acb3cdecff2578ed0
josiedpb14 <110139188+josiedpb14@users.noreply.github.com>
2023-05-08 03:12:04 +0000

    Main.js created with all the arrays and the excuse function

---
+++ src/main.js
@@ -0,0 +1,24 @@
+let who = [
+  "My mom",
+  "Jeffry",
+  "My Dog",
+  "Mariano",
+  "A warewolf",
+  "My neighbor",
+];
+let action = ["stole", "scratched", "made", "drank", "lost", "broke"];
+let what = ["my phone", "your car", "a mess", "coke", "its prey", "the window"];
+let when = [
+  "while i was cooking",
+  "When you werent looking",
+  "during the evening",
+  "while we werent looking",
+  "when was asleep",
+  "when no one could see him",
+];
+
+function makeAnExcuse() {
+  let num = Math.floor(Math.random() * 6) + 1;
+  let excuse = `${who[num]} ${action[num]} ${what[num]} ${when[num]} `;
+  return;
+}
